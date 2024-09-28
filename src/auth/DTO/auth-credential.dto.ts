44df0a2130aa499c0448389610856a15f237f9f3
윤재형 <yoonwogud@gmail.com>
2024-09-29 01:02:35 +0900

    ㅁㄴㅇㄹ

--- src/auth/DTO/auth-credential.dto.ts
+++ src/auth/DTO/auth-credential.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsNotEmpty,
   IsString,
@@ -10,10 +11,18 @@ export class AuthCredentialsDto {
   @IsString()
   @MinLength(4)
   @MaxLength(20)
+  @ApiProperty({
+    example: 'user123',
+    description: '유저 아이디',
+  })
   username: string;
   @IsString()
   @MinLength(4)
   @MaxLength(20)
+  @ApiProperty({
+    example: 'pass123',
+    description: '유저 비밀번호',
+  })
   @Matches(/^[a-zA-Z0-9]*$/, {
     message: '비밀번호는 영어와 숫자만 가능합니다.',
   }) //영어랑 숫자만 가능한 유효성 체크
